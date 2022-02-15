d92b7a4e577525918a5afeec64bf5b90c5e77a5e
Dirk Weissmann <Dirk Weissmann@INV241440.INFO.TEAM>
2022-02-15 15:44:22 +0100

    The changes for Step 2
    - Included Lombok
    - Included Spring Dev Tools

--- src/main/java/de/infoteam/PetshopWebApp.java
+++ src/main/java/de/infoteam/PetshopWebApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenAPI2SpringBoot {
+public class PetshopWebApp {
 	public static void main(final String[] args) {
-		new SpringApplication(OpenAPI2SpringBoot.class).run(args);
+		new SpringApplication(PetshopWebApp.class).run(args);
 	}
 }
