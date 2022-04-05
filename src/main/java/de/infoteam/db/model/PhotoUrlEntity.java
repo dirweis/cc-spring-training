9711f52f88dd548c88044e3d607dea0ec56a9aa8
Dirk Weissmann <dirk.weissmann@infoteam.de>
2022-04-05 17:28:29 +0200

    Removed modified timestamp in entities

--- src/main/java/de/infoteam/db/model/PhotoUrlEntity.java
+++ src/main/java/de/infoteam/db/model/PhotoUrlEntity.java
@@ -16,7 +16,6 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
@@ -51,10 +50,6 @@ public class PhotoUrlEntity {
 	@Column(nullable = false, updatable = false)
 	@CreatedDate
 	private Date createdTime;
-
-	@Column(nullable = false)
-	@LastModifiedDate
-	private Date modifiedTime;
 
 	/**
 	 * The constructor for adding a new entity on the given URL.
