fe2ce773c29d0e1ddfd55bbd3849e1c67e354b51
Dirk Weissmann <50779382+dirweis@users.noreply.github.com>
2022-04-05 16:04:32 +0200

    Update Pet.java

--- src/main/java/de/infoteam/model/Pet.java
+++ src/main/java/de/infoteam/model/Pet.java
@@ -1,6 +1,6 @@
 package de.infoteam.model;
 
-import java.net.URI;
+import java.net.URL;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record Pet(UUID id, @NotNull Category category, @NotNull @Size(min = 3, max = 30) String name,
-		@JsonProperty("photo-urls") List<URI> photoUrls, List<@Size(min = 3, max = 20) String> tags,
+		@JsonProperty("photo-urls") List<URL> photoUrls, List<@Size(min = 3, max = 20) String> tags,
 		@NotNull PetStatus status, @NotNull @Size(min = 30, max = 1_000) String description) {
 
 	public enum Category {
