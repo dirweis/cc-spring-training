fd44f3dbfeae3dcbe825863b0a941eef470d2b8d
Dirk Weissmann <50779382+dirweis@users.noreply.github.com>
2022-04-05 16:07:56 +0200

    Update Pet.java

--- src/main/java/de/infoteam/model/Pet.java
+++ src/main/java/de/infoteam/model/Pet.java
@@ -1,6 +1,6 @@
 package de.infoteam.model;
 
-import java.net.URI;
+import java.net.URL;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,17 +19,48 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @param id          the ID, may be {@code null} (in case of a POST request, the ID must be {@code null})
  * @param category    the pet's category, must not be {@code null}
  * @param name        the pet's name, must not be {@code null}
- * @param photoUrls   the {@link List} of {@link URI}s that link to an image, may be {@code null}
+ * @param photoUrls   the {@link List} of {@link URL}s that link to an image, may be {@code null}
  * @param tags        a {@link List} of {@link String}s as tags for the {@link Pet}, may be {@code null}
  * @param status      the {@link Pet}'s status from {@link PetStatus}, must not be {@code null}
  * @param description the description of the {@link Pet}, must not be {@code null}
  */
 public record Pet(UUID id, @NotNull Category category, @NotNull @Size(min = 3, max = 30) String name,
-		@JsonProperty("photo-urls") List<URI> photoUrls, List<@Size(min = 3, max = 20) String> tags,
+		@JsonProperty("photo-urls") List<URL> photoUrls, List<@Size(min = 3, max = 20) String> tags,
 		@NotNull PetStatus status, @NotNull @Size(min = 30, max = 1_000) String description) {
 
-	private enum Category {
-		DOG, CAT, BIRD, MOUSE, SPIDER;
+	/**
+	 * A little enumeration for a {@link Pet}'s category.
+	 * 
+	 * @author Dirk Weissmann
+	 * @since 2022-02-16
+	 * @version 1.0
+	 *
+	 */
+	public enum Category {
+		/**
+		 * the category representing the {@link String} {@code DOG}
+		 */
+		DOG,
+
+		/**
+		 * the category representing the {@link String} {@code CAT}
+		 */
+		CAT,
+
+		/**
+		 * the category representing the {@link String} {@code BIRD}
+		 */
+		BIRD,
+
+		/**
+		 * the category representing the {@link String} {@code MOUSE}
+		 */
+		MOUSE,
+
+		/**
+		 * the category representing the {@link String} {@code SPIDER}
+		 */
+		SPIDER;
 	}
 
 	/**
