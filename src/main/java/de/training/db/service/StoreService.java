90c2cb2d2df312c4ec9ab402bd517204efc2d49d
dirwe <dirwe@LAPTOP-3FBRC6MH.fritz.box>
2023-08-02 21:03:07 +0200

    Upgrade to Spring Boot 3

--- src/main/java/de/training/db/service/StoreService.java
+++ src/main/java/de/training/db/service/StoreService.java
@@ -3,11 +3,6 @@ package de.training.db.service;
 import java.util.List;
 import java.util.UUID;
 
-import javax.persistence.EntityNotFoundException;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import org.mapstruct.Mapper;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -23,6 +18,10 @@ import de.training.db.model.TagEntity;
 import de.training.model.Pet;
 import de.training.model.Pet.Category;
 import de.training.model.Pet.PetStatus;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -38,107 +37,109 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class StoreService {
 
-	private final PetRepositoryDao petRepository;
+    private final PetRepositoryDao petRepository;
 
-	private final PetMapper mapper;
+    private final PetMapper mapper;
 
-	/**
-	 * Stores a new {@link Pet} resource into the database. The {@link PetMapper} is used for mapping the {@link Pet} to
-	 * a {@link PetEntity}. Then the {@link PetRepositoryDao} is used for the storing itself.
-	 * 
-	 * @param petDto the {@link Pet} resources to be stored
-	 * 
-	 * @return the stored entity's ID
-	 */
-	public UUID storeNewPet(final Pet petDto) {
-		final PetEntity entity = mapper.dto2Entity(petDto);
+    /**
+     * Stores a new {@link Pet} resource into the database. The {@link PetMapper} is used for mapping the {@link Pet} to
+     * a {@link PetEntity}. Then the {@link PetRepositoryDao} is used for the storing itself.
+     * 
+     * @param petDto the {@link Pet} resources to be stored
+     * 
+     * @return the stored entity's ID
+     */
+    public UUID storeNewPet(final Pet petDto) {
+        final PetEntity entity = mapper.dto2Entity(petDto);
 
-		linkTags(entity);
+        StoreService.linkTags(entity);
 
-		petRepository.save(entity);
+        petRepository.save(entity);
 
-		return entity.getId();
-	}
+        return entity.getId();
+    }
 
-	/**
-	 * Selects a {@link PetEntity} database entry by its ID and maps it into a {@link Pet} DTO. In case of a not found
-	 * ID, an {@link EntityNotFoundException} is thrown and handled in the {@link ExceptionHandler}.
-	 * 
-	 * @param petId the {@link PetEntity}'s ID, may be {@code null}
-	 * 
-	 * @return the mapped DTO
-	 */
-	@Transactional(readOnly = true)
-	public Pet getPetById(final UUID petId) {
-		final PetEntity entity = petRepository.findById(petId).orElseThrow(
-				() -> new EntityNotFoundException("Resource with ID " + petId + " not found in the persistence"));
+    /**
+     * Selects a {@link PetEntity} database entry by its ID and maps it into a {@link Pet} DTO. In case of a not found
+     * ID, an {@link EntityNotFoundException} is thrown and handled in the {@link ExceptionHandler}.
+     * 
+     * @param petId the {@link PetEntity}'s ID, may be {@code null}
+     * 
+     * @return the mapped DTO
+     */
+    @Transactional(readOnly = true)
+    public Pet getPetById(final UUID petId) {
+        final PetEntity entity = petRepository.findById(petId).orElseThrow(
+                () -> new EntityNotFoundException("Resource with ID " + petId + " not found in the persistence"));
 
-		return mapper.entity2Dto(entity);
-	}
+        return mapper.entity2Dto(entity);
+    }
 
-	/**
-	 * Retrieves a result set of {@link PetEntity} objects by the given parameters.
-	 * 
-	 * @param status   the {@link PetStatus} as first {@code WHERE} parameter, may be {@code null}
-	 * @param tags     the {@link List} of tags as second {@code WHERE} parameter, may be {@code null}
-	 * @param category the {@link Category} of the {@link Pet}, may be {@code null}
-	 * @param pageable the paging parameter, may be {@code null}
-	 * 
-	 * @return the {@link List} of {@link Pet} DTOs after mapping the DB result
-	 */
-	@Transactional(readOnly = true)
-	public List<Pet> findByParameters(final List<String> tags, final PetStatus status, final Category category,
-			final Pageable pageable) {
+    /**
+     * Retrieves a result set of {@link PetEntity} objects by the given parameters.
+     * 
+     * @param status   the {@link PetStatus} as first {@code WHERE} parameter, may be {@code null}
+     * @param tags     the {@link List} of tags as second {@code WHERE} parameter, may be {@code null}
+     * @param category the {@link Category} of the {@link Pet}, may be {@code null}
+     * @param pageable the paging parameter, may be {@code null}
+     * 
+     * @return the {@link List} of {@link Pet} DTOs after mapping the DB result
+     */
+    @Transactional(readOnly = true)
+    public List<Pet> findByParameters(final List<String> tags, final PetStatus status, final Category category,
+            final Pageable pageable) {
 
-		final List<PetEntity> entities = petRepository
-				.findAll(hasStatus(status).and(hasCategory(category).and(isInTags(tags))), pageable).getContent();
+        final List<PetEntity> entities = petRepository
+                .findAll(StoreService.hasStatus(status)
+                        .and(StoreService.hasCategory(category).and(StoreService.isInTags(tags))), pageable)
+                .getContent();
 
-		return entities.stream().map(mapper::entity2Dto).toList();
-	}
+        return entities.stream().map(mapper::entity2Dto).toList();
+    }
 
-	/**
-	 * The crazy JPA "feature": Even though the {@link TagEntity} objects are already allocated to the
-	 * {@link PetEntity}, it must also be done the other way.
-	 * 
-	 * @param petEntity the {@link PetEntity} for allocation
-	 */
-	private static void linkTags(final PetEntity petEntity) {
-		petEntity.getTags().forEach(tagEntity -> tagEntity.setPet(petEntity));
-	}
+    /**
+     * The crazy JPA "feature": Even though the {@link TagEntity} objects are already allocated to the
+     * {@link PetEntity}, it must also be done the other way.
+     * 
+     * @param petEntity the {@link PetEntity} for allocation
+     */
+    private static void linkTags(final PetEntity petEntity) {
+        petEntity.getTags().forEach(tagEntity -> tagEntity.setPet(petEntity));
+    }
 
-	/**
-	 * The {@link Specification} for enriching the {@code WHERE} statement with a check on the {@link PetStatus}.
-	 * 
-	 * @param status the {@link PetStatus}, may be {@code null}
-	 * 
-	 * @return the {@link Specification} for restrictions or {@code null} in case it's not needed
-	 */
-	private static Specification<PetEntity> hasStatus(final PetStatus status) {
-		return (final Root<PetEntity> root, final CriteriaQuery<?> cq,
-				final CriteriaBuilder cb) -> status != null ? cb.equal(root.get("status"), status) : null;
-	}
+    /**
+     * The {@link Specification} for enriching the {@code WHERE} statement with a check on the {@link PetStatus}.
+     * 
+     * @param status the {@link PetStatus}, may be {@code null}
+     * 
+     * @return the {@link Specification} for restrictions or {@code null} in case it's not needed
+     */
+    private static Specification<PetEntity> hasStatus(final PetStatus status) {
+        return (final Root<PetEntity> root, final CriteriaQuery<?> cq,
+                final CriteriaBuilder cb) -> status != null ? cb.equal(root.get("status"), status) : null;
+    }
 
-	/**
-	 * The {@link Specification} for enriching the {@code WHERE} statement with a check on the {@link Category}.
-	 * 
-	 * @param category the {@link Pet}'s {@link Category}, may be {@code null}
-	 * 
-	 * @return the {@link Specification} for restrictions or {@code null} in case it's not needed
-	 */
-	private static Specification<PetEntity> hasCategory(final Category category) {
-		return (final Root<PetEntity> root, final CriteriaQuery<?> cq,
-				final CriteriaBuilder cb) -> category != null ? cb.equal(root.get("category"), category) : null;
-	}
+    /**
+     * The {@link Specification} for enriching the {@code WHERE} statement with a check on the {@link Category}.
+     * 
+     * @param category the {@link Pet}'s {@link Category}, may be {@code null}
+     * 
+     * @return the {@link Specification} for restrictions or {@code null} in case it's not needed
+     */
+    private static Specification<PetEntity> hasCategory(final Category category) {
+        return (final Root<PetEntity> root, final CriteriaQuery<?> cq,
+                final CriteriaBuilder cb) -> category != null ? cb.equal(root.get("category"), category) : null;
+    }
 
-	/**
-	 * The {@link Specification} for enriching the {@code WHERE} statement with a check on given tags..
-	 * 
-	 * @param tags the {@link List} of tag {@link String}s, may be {@code null}
-	 * 
-	 * @return the {@link Specification} for restrictions or {@code null} in case it's not needed
-	 */
-	private static Specification<PetEntity> isInTags(final List<String> tags) {
-		return (final Root<PetEntity> root, final CriteriaQuery<?> cq,
-				final CriteriaBuilder cb) -> tags != null ? cb.in(root.join("tags").get("tag")).value(tags) : null;
-	}
+    /**
+     * The {@link Specification} for enriching the {@code WHERE} statement with a check on given tags..
+     * 
+     * @param tags the {@link List} of tag {@link String}s, may be {@code null}
+     * 
+     * @return the {@link Specification} for restrictions or {@code null} in case it's not needed
+     */
+    private static Specification<PetEntity> isInTags(final List<String> tags) {
+        return (final Root<PetEntity> root, final CriteriaQuery<?> cq,
+                final CriteriaBuilder cb) -> tags != null ? cb.in(root.join("tags").get("tag")).value(tags) : null;
+    }
 }
--- src/test/java/de/training/restriction/WebControllerContraintsInDeleteTest.java
+++ src/test/java/de/training/restriction/WebControllerContraintsInDeleteTest.java
@@ -26,42 +26,42 @@ import lombok.SneakyThrows;
  * 
  * @author Dirk Weissmann
  * @since 2022-02-22
- * @version 1.1
+ * @version 1.2
  *
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @DisplayName("WHEN the DELETE endpoint is called with")
 class WebControllerContraintsInDeleteTest extends AbstractSpringTestRunner {
 
-	/**
-	 * Tests the {@code DELETE} endpoint while calling it with the falsely given {@code POST} method.
-	 */
-	@Test
-	@SneakyThrows
-	@DisplayName("a wrong HTTP method THEN respond with status 405 AND content type application/problem+json AND the expected response body")
-	void testWrongHttpMethodAndExpect405() {
-		mockMvc.perform(post(EndPointWithTestId)).andExpect(status().isMethodNotAllowed())
-				.andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
-				.andExpect((final MvcResult result) -> assertThat(result.getResolvedException())
-						.isInstanceOf(HttpRequestMethodNotSupportedException.class))
-				.andExpect(content().string(containsString("\"title\":\"Request method 'POST' not supported\"")))
-				.andExpect(content().string(containsString("\"detail\":\"Supported method(s): [GET, PUT, DELETE]\"")));
-	}
+    /**
+     * Tests the {@code DELETE} endpoint while calling it with the falsely given {@code POST} method.
+     */
+    @Test
+    @SneakyThrows
+    @DisplayName("a wrong HTTP method THEN respond with status 405 AND content type application/problem+json AND the expected response body")
+    void testWrongHttpMethodAndExpect405() {
+        mockMvc.perform(post(EndPointWithTestId)).andExpect(status().isMethodNotAllowed())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                .andExpect((final MvcResult result) -> assertThat(result.getResolvedException())
+                        .isInstanceOf(HttpRequestMethodNotSupportedException.class))
+                .andExpect(content().string(containsString("\"title\":\"Request method 'POST' is not supported\"")))
+                .andExpect(content().string(containsString("\"detail\":\"Supported method(s): [")));
+    }
 
-	/**
-	 * Tests the {@code DELETE} endpoint while calling it with an invalid ID (invalid format).
-	 */
-	@Test
-	@SneakyThrows
-	@DisplayName("an ID that is not well-formed THEN respond with status 400 AND content type application/problem+json AND the expected response body")
-	void testInvalidPetIdAndExpect400() {
-		mockMvc.perform(delete(EndPointPrefix + "/no")).andExpect(status().isBadRequest())
-				.andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
-				.andExpect((final MvcResult result) -> assertThat(result.getResolvedException())
-						.isInstanceOf(MethodArgumentTypeMismatchException.class))
-				.andExpect(content().string(containsString(
-						"\"title\":\"Failed to convert value of type 'String' to required type 'UUID'\"")))
-				.andExpect(content().string(containsString(
-						"\"invalid_params\":[{\"name\":\"petId\",\"reason\":\"Invalid UUID string: no\"}]")));
-	}
+    /**
+     * Tests the {@code DELETE} endpoint while calling it with an invalid ID (invalid format).
+     */
+    @Test
+    @SneakyThrows
+    @DisplayName("an ID that is not well-formed THEN respond with status 400 AND content type application/problem+json AND the expected response body")
+    void testInvalidPetIdAndExpect400() {
+        mockMvc.perform(delete(EndPointPrefix + "/no")).andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                .andExpect((final MvcResult result) -> assertThat(result.getResolvedException())
+                        .isInstanceOf(MethodArgumentTypeMismatchException.class))
+                .andExpect(content().string(containsString(
+                        "\"title\":\"Failed to convert value of type 'String' to required type 'UUID'\"")))
+                .andExpect(content().string(containsString(
+                        "\"invalid_params\":[{\"name\":\"petId\",\"reason\":\"Invalid UUID string: no\"}]")));
+    }
 }
